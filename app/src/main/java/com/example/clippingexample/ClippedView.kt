e9a1950d9846966f8297f998607277045983a739
Damian <damian.ogorek.1996@gmail.com>
2020-06-11 21:26:40 +0200

    Setted up the shapes.

--- app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -20,4 +20,17 @@ class ClippedView @JvmOverloads constructor(
     //To store locally the path of what has been drawn
     private val path = Path()
 
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset);
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
 }
